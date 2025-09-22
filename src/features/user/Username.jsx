6cd7cd5fe6bff65ae2aee685f5ad599d2f6bf05c
Soheil <iamsoheil.r@gmail.com>
2025-09-22 21:12:18 +0330

    Add spacing to the ui

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Soheil</div>;
+  return <div className="hidden text-sm font-semibold">Soheil</div>;
 }
 
 export default Username;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,26 +1,25 @@
-import Header from "./Header";
-import Loader from "./Loader";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
-    const navigation = useNavigation();
-    const isLoading = navigation.state === "loading";
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
 
-    return (
-        <div className="layout">
-            {isLoading && <Loader />}
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
 
-            <Header />
+      <Header />
 
-            <main>
-                <h1>Content</h1>
-                <Outlet />
-            </main>
+      <main>
+        <Outlet />
+      </main>
 
-            <CartOverview />
-        </div>
-    );
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
