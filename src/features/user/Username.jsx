d797771ed16bb51da7a939a284d2b8fb6b370af7
Soheil <iamsoheil.r@gmail.com>
2025-09-23 09:14:31 +0330

    Add flexbox to the ui

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Soheil</div>;
+  return <div className="hidden text-sm font-semibold md:block">Soheil</div>;
 }
 
 export default Username;
