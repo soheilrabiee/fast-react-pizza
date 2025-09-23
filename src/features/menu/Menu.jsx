11b0b50e248009e504db669f08146f286838dbd6
Soheil <iamsoheil.r@gmail.com>
2025-09-23 19:03:48 +0330

    Style the menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,23 +1,23 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
-    const menu = useLoaderData();
+  const menu = useLoaderData();
 
-    return (
-        <ul>
-            {menu.map((pizza) => (
-                <MenuItem pizza={pizza} key={pizza.id} />
-            ))}
-        </ul>
-    );
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 // Loader function for the route
 export async function loader() {
-    const menu = await getMenu();
-    return menu;
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
